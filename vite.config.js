cd78f6533c9f72eea75ae4add204a1daef122139
AutumnSaury <autumnsaury@outlook.com>
2022-04-01 14:59:44 +0800

    configured proxy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  server: {
+    port: 3777,
+    proxy: {
+      '/3rd/bd_auth': {
+        target: 'https://aip.baidubce.com/oauth/2.0/token',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/3rd\/bd_auth/, '')
+      },
+      '/3rd/bd_bkimg': {
+        target: 'https://bkimg.cdn.bcebos.com/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/3rd\/bd_bkimg/, '')
+      },
+      '3rd/bd_plant': {
+        target: 'https://aip.baidubce.com/rest/2.0/image-classify/v1/plant',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/3rd\/bd_palnt/, '')
+      },
+    }
+  }
 })
